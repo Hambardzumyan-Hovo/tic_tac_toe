4bb6dec63317ebf1e280c0a595f88793dfd1e7b6
thedarkknight1990 <hovhannessyan@gmail.com>
2020-08-05 23:09:40 +0400

    Updates

---
+++ precache-manifest.58d1c78d69fe53b8ea1ca704ecf49f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b99aadcec8096f19f66272d9ada7d4c",
+    "url": "/tic_tac_toe/index.html"
+  },
+  {
+    "revision": "9b414fece52e4510ce7d",
+    "url": "/tic_tac_toe/static/css/main.f6b1c9a9.chunk.css"
+  },
+  {
+    "revision": "734fa3ba9ed80d61a1a1",
+    "url": "/tic_tac_toe/static/js/2.99d88f84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic_tac_toe/static/js/2.99d88f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b414fece52e4510ce7d",
+    "url": "/tic_tac_toe/static/js/main.f5e45682.chunk.js"
+  },
+  {
+    "revision": "135ba7b087ac225cebf9",
+    "url": "/tic_tac_toe/static/js/runtime-main.49020b15.js"
+  }
+]);
