16df09c518bf7809b376513b7b2418cc9c09ca52
thedarkknight1990 <hovhannessyan@gmail.com>
2021-02-10 21:34:24 +0400

    Updates

--- precache-manifest.aa8a62e779d87d1399df0abafbcb9123.js
+++ precache-manifest.aa8a62e779d87d1399df0abafbcb9123.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2dc9d5a3cc6aee9a631cb04b5da6c64c",
+    "revision": "72859656e81f322d34a8a671577b3830",
     "url": "/tic_tac_toe/index.html"
   },
   {
-    "revision": "645d8b2fca1fa51e2dbf",
+    "revision": "82839dc367cef56de52f",
     "url": "/tic_tac_toe/static/css/main.6bc365b0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tic_tac_toe/static/js/2.99d88f84.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "645d8b2fca1fa51e2dbf",
-    "url": "/tic_tac_toe/static/js/main.0b7c596d.chunk.js"
+    "revision": "82839dc367cef56de52f",
+    "url": "/tic_tac_toe/static/js/main.be61db15.chunk.js"
   },
   {
     "revision": "135ba7b087ac225cebf9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic_tac_toe/precache-manifest.9adb17850be5602df0370ed30f7f5614.js"
+  "/tic_tac_toe/precache-manifest.aa8a62e779d87d1399df0abafbcb9123.js"
 );
 
 self.addEventListener('message', (event) => {
